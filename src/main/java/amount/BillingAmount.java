a39af595044e65f673059586138470d50a6fe33d
mmm00mmm <54709088+mmm00mmm@users.noreply.github.com>
2020-01-03 21:44:43 +0900

    請求金額クラスを新規作成

---
+++ src/main/java/amount/BillingAmount.java
@@ -0,0 +1,13 @@
+package amount;
+
+/**
+ * 請求金額
+ */
+public class BillingAmount {
+    private final int value; //TODO:intでいいのかな
+
+    //TODO:intでいいのかな
+    public BillingAmount(int value) {
+        this.value = value;
+    }
+}
