bd77445a3acfd8467bfaeb6657675f8a573cb387
mi-yamashita <m-yamashita@cnt.biglobe.co.jp>
2020-01-22 08:55:40 +0900

    streamを使えるようにする

--- src/main/java/domain/model/member/MemberList.java
+++ src/main/java/domain/model/member/MemberList.java
@@ -1,11 +1,16 @@
 package domain.model.member;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class MemberList {
     private final List<Member> values;
 
     public MemberList(List<Member> memberList) {
         this.values = memberList;
+    }
+
+    public Stream<Member> stream() {
+        return values.stream();
     }
 }
