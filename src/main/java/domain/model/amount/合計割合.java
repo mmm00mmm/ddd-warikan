200f4472316f06780b6309c0b4e4a0366ea0ec30
mi-yamashita <m-yamashita@cnt.biglobe.co.jp>
2020-01-30 12:55:19 +0900

    合計割合メソッドを修正

--- src/main/java/domain/model/amount/合計割合.java
+++ src/main/java/domain/model/amount/合計割合.java
@@ -1,10 +1,27 @@
 package domain.model.amount;
 
+import domain.model.member.Member;
 import domain.model.member.MemberList;
+import domain.model.member.PaymentType;
 
 public class 合計割合 {
-    public 合計割合 算出する(MemberList memberList) {
-        //memberList.stream().map(Member::getPaymentType)
-        return null;
+    private final Integer value;
+
+    public 合計割合(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    public static 合計割合 算出する(MemberList memberList) {
+        return new 合計割合(
+                memberList.stream()
+                        .map(Member::getPaymentType)
+                        .mapToInt(PaymentType::getValue)
+                        .sum()
+        );
     }
 }
