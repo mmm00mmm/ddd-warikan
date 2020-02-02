d06f5cc9e1cf4a6970b3442c6e0d262ab7cc7ca1
mmm00mmm <54709088+mmm00mmm@users.noreply.github.com>
2020-02-02 20:34:44 +0900

    新クラス追加

---
+++ src/main/java/domain/model/amount/合計支払金額.java
@@ -0,0 +1,14 @@
+package domain.model.amount;
+
+public class 合計支払金額 {
+    private final Integer value;
+
+    public 合計支払金額(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "合計支払金額:" + value + "円";
+    }
+}
